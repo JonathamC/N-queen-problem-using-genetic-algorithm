a5f59f3054f4849aa071337c3e660e0264294cb4
JonathamC <86174437+JonathamC@users.noreply.github.com>
2022-06-18 22:18:19 -0400

    Update n_queen_problem.py

--- n_queen_problem.py
+++ n_queen_problem.py
@@ -1,21 +1,21 @@
 import random
 from time import sleep 
-def generateStartingPopulation(size): 
+def generateStartingPopulation(size, numQueens): 
     """ 
     Generate starting population
     """
     populations = [] 
     for _ in range(size): 
         temp = []
-        for q in range(8): 
-            temp.append(random.randrange(0,8))
+        for q in range(numQueens): 
+            temp.append(random.randrange(0,numQueens))
         
         populations.append(temp)
     
     return populations
 
-def convertTo2DMatrix(chromosome):
-    chessboard = [["-" for _ in range(8)] for _ in range(8)]
+def convertTo2DMatrix(chromosome, numQueens):
+    chessboard = [["-" for _ in range(numQueens)] for _ in range(numQueens)]
     column = 0
     for row in chromosome: 
         chessboard[row][column] = "Q"
@@ -28,40 +28,39 @@ def printMatrix(matrix):
             print("{}  ".format(j), end="")
         print()
 
-def topDownDiagonalMatrixTraversal(matrix): 
+def topDownDiagonalMatrixTraversal(matrix, numQueens): 
     """
     Traverse matrix from top left to bottom right corner. 
     """
     result = [] 
 
-    for l in range(2*8-1): 
-        t = max(0, l+1-8)
-        b = min(l, 8-1)
+    for l in range(2*numQueens-1): 
+        t = max(0, l+1-numQueens)
+        b = min(l, numQueens-1)
         temp = []
         for i in range(t, b + 1): 
             temp.append(matrix[i][l-i])
         result.append(temp)
     return result
 
-def bottomUpDiagonalMatrixTraversal(chromosome): 
+def bottomUpDiagonalMatrixTraversal(chromosome, numQueens): 
     """
     Traverse matrix from top right to bottom left corner. 
-
     To achieve this result without coding new traversal method,
     just simply flip the matrix horizontally and call TopDownDiagonalMatrixTraversal. 
     """
     revChromosome = chromosome[::-1]
-    revMatrix = convertTo2DMatrix(revChromosome)
-    return topDownDiagonalMatrixTraversal(revMatrix)
+    revMatrix = convertTo2DMatrix(revChromosome, numQueens)
+    return topDownDiagonalMatrixTraversal(revMatrix, numQueens)
 
-def topDownVerticalMatrixTraversal(matrix): 
+def topDownVerticalMatrixTraversal(matrix, numQueens): 
     """
     Traverse matrix from top to bottom vertically. 
     """
     result = []
-    for i in range(8): 
+    for i in range(numQueens): 
         temp = [] 
-        for j in range(8): 
+        for j in range(numQueens): 
             temp.append(matrix[j][i])
         result.append(temp)
     return result
@@ -69,7 +68,6 @@ def topDownVerticalMatrixTraversal(matrix):
 def countQueensAttacking(matrix): 
     """ 
     Count number of queens attacking in matrix. 
-
     Used in calculating fitness score. 
     """
     total = 0 
@@ -83,35 +81,35 @@ def countQueensAttacking(matrix):
         
     return total 
 
-def crossover(p1, p2): 
+def crossover(p1, p2, numQueens): 
     """
     Reproduce using parent1 p1 chromosome and parent2 p2 chromosome by crossover. 
     Random crossover points between 1 and 7 
     """
 
-    crossoverPoint = random.randrange(1,8)
+    crossoverPoint = random.randrange(1,numQueens)
     child1 = p1[:crossoverPoint] + p2[crossoverPoint:]
     child2 = p2[:crossoverPoint] + p1[crossoverPoint:]
 
     return child1, child2
 
 
-def fitness(chromosome): 
+def fitness(chromosome, numQueens): 
     """
     Count how many queens are attacking horizontally and diagonally. 
     """
     fitnessScore = 0
-    chessboard = convertTo2DMatrix(chromosome)
+    chessboard = convertTo2DMatrix(chromosome, numQueens)
 
     # horizontally 
     fitnessScore += countQueensAttacking(chessboard)
 
     # vertically 
-    fitnessScore += countQueensAttacking(topDownVerticalMatrixTraversal(chessboard))
+    fitnessScore += countQueensAttacking(topDownVerticalMatrixTraversal(chessboard, numQueens))
 
     # diagonally 
-    fitnessScore += countQueensAttacking(topDownDiagonalMatrixTraversal(chessboard))
-    fitnessScore += countQueensAttacking(bottomUpDiagonalMatrixTraversal(chromosome))
+    fitnessScore += countQueensAttacking(topDownDiagonalMatrixTraversal(chessboard, numQueens))
+    fitnessScore += countQueensAttacking(bottomUpDiagonalMatrixTraversal(chromosome, numQueens))
 
     return fitnessScore
 
@@ -121,26 +119,27 @@ def randomIndexGenerator(selectionFactor):
 
     return int(randIndex)
 
-def mutation(chromosome, mutationProb): 
-    if random.uniform(0,1) < 0.6: 
-        mutatePoint = random.randrange(0,8)
-        mutate = random.randrange(0,8)
+def mutation(chromosome, mutationProb, numQueens): 
+    if random.uniform(0,1) < mutationProb: 
+        mutatePoint = random.randrange(0,numQueens)
+        mutate = random.randrange(0,numQueens)
 
         chromosome[mutatePoint] = mutate
 
 
 def main():
     generations = 1
-    selectionFactor = 5 # randomly chosen number. The higher the selection factor, the better for parents selection. 
-    populationSize = 150 # randomly chosen 
-    mutationProb = 0.6 #randomly chosen 
-
+    populationSize = 150 # chosen value
+    numQueens = int(input("Enter number of queens: "))
+    selectionFactor = 5 # chosen value 
+    mutationProb = 0.6  # chosen value 
+    print("\n")
     # generate starting population
-    population = generateStartingPopulation(populationSize)
+    population = generateStartingPopulation(populationSize, numQueens)
     populationWithFitness = []
 
     for i in population: 
-        populationWithFitness.append([fitness(i), i])
+        populationWithFitness.append([fitness(i, numQueens), i])
     # create a new population 2d array with fitness score for each chromosome
     populationWithFitness = sorted(populationWithFitness, key = lambda x: x[0], reverse = False)
 
@@ -148,7 +147,7 @@ def main():
     while (True):
 
         tempPopulation = []
-        for _ in range(75): 
+        for _ in range(populationSize // 2): 
             # parent1 and parent2 selection 
             p1 = populationWithFitness[randomIndexGenerator(selectionFactor)][1]
             p2 = populationWithFitness[randomIndexGenerator(selectionFactor)][1]
@@ -158,24 +157,24 @@ def main():
                 p2 = populationWithFitness[randomIndexGenerator(selectionFactor)][1]
 
 
-            child1, child2 = crossover(p1, p2)
-            mutation(child1, mutationProb)
-            mutation(child2, mutationProb)
-            fitnessChild1 = fitness(child1)
-            fitnessChild2 = fitness(child2)
+            child1, child2 = crossover(p1, p2, numQueens)
+            mutation(child1, mutationProb, numQueens)
+            mutation(child2, mutationProb, numQueens)
+            fitnessChild1 = fitness(child1, numQueens)
+            fitnessChild2 = fitness(child2, numQueens)
             
             
 
 
             if fitnessChild1 == 0: 
                 print("\nGeneration #: {}\n".format(generations))
-                printMatrix(convertTo2DMatrix(child1))
+                printMatrix(convertTo2DMatrix(child1, numQueens))
                 print("Chromosome: {}, Fitness Score = {}\n".format(child1, fitnessChild1))
                 return 
             print("Chromosome = {}, Fitness Score = {}".format(child1, fitnessChild1))
             if fitnessChild2 == 0: 
                 print("\nGeneration #: {}\n".format(generations))
-                printMatrix(convertTo2DMatrix(child2))
+                printMatrix(convertTo2DMatrix(child2, numQueens))
                 print("Chromosome: {}, Fitness Score = {}\n".format(child2, fitnessChild2))
                 return
             print("Chromosome = {}, Fitness Score = {}".format(child2, fitnessChild2))
